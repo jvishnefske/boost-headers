9d1c0912227b2bd02406d8c670f260c5e246404f
John Vishnefske <jvishnefske@gmail.com>
2022-07-21 12:33:42 -0500

    respect asio, and beast preprocessor definition for seperate library

--- seperate_compilation/libs.cpp
+++ seperate_compilation/libs.cpp
@@ -1,8 +1,13 @@
-#define BOOST_BEAST_SEPARATE_COMPILATION 1
-#define BOOST_ASIO_SEPARATE_COMPILATION 1
-
-#include <boost/asio/impl/src.hpp>
-// this only works if openssl is available.
-//#include <boost/asio/ssl/impl/src.hpp>
+#if BOOST_BEAST_SEPARATE_COMPILATION
 #include <boost/beast/src.hpp>
+#endif
+
+#if BOOST_ASIO_SEPARATE_COMPILATION
+#include <boost/asio/impl/src.hpp>
+#if BOOST_ASIO_USE_OPENSSL
+// this only works if openssl is available.
+#include <boost/asio/ssl/impl/src.hpp>
+#endif
+#endif
+
 #include <boost/json/src.hpp>
