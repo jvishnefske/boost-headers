f38650efac6e014a4f918e155144e5baf4b2c573
John Vishnefske <jvishnefske@gmail.com>
2022-07-29 12:34:16 -0500

    add example imports

---
+++ separate_compilation/boost_example.hpp
@@ -0,0 +1,136 @@
+//
+// Created by vis75817 on 7/29/2022.
+//
+
+#ifndef THIRD_PARTY_BOOST_HEADERS_SEPARATE_COMPILATION_EXAMPLE_HPP_
+#define THIRD_PARTY_BOOST_HEADERS_SEPARATE_COMPILATION_EXAMPLE_HPP_
+
+/**
+ * this is a non exhaustive list of headers which may be included from this repo
+ * it includes convienince headers with some that create warnings commented out.
+ */
+
+#include <boost/align.hpp>
+#include <boost/aligned_storage.hpp>
+#include <boost/any.hpp>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/assert.hpp>
+#include <boost/assign.hpp>
+#include <boost/atomic.hpp>
+#include <boost/beast.hpp>
+#include <boost/bimap.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/blank.hpp>
+#include <boost/blank_fwd.hpp>
+#include <boost/call_traits.hpp>
+#include <boost/callable_traits.hpp>
+#include <boost/cast.hpp>
+#include <boost/cerrno.hpp>
+#include <boost/checked_delete.hpp>
+//#include <boost/chrono.hpp>
+#include <boost/circular_buffer.hpp>
+#include <boost/circular_buffer_fwd.hpp>
+#include <boost/compressed_pair.hpp>
+// compute requires openCL
+//#include <boost/compute.hpp>
+#include <boost/concept_archetype.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/config.hpp>
+#include <boost/contract.hpp>
+#include <boost/contract_macro.hpp>
+#include <boost/convert.hpp>
+#include <boost/crc.hpp>
+//#include <boost/cregex.hpp>
+#include <boost/cstdfloat.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/cstdlib.hpp>
+#include <boost/current_function.hpp>
+#include <boost/cxx11_char_types.hpp>
+#include <boost/date_time.hpp>
+#include <boost/describe.hpp>
+#include <boost/dll.hpp>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/dynamic_bitset_fwd.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/endian.hpp>
+#include <boost/exception_ptr.hpp>
+//#include <boost/filesystem.hpp>
+#include <boost/flyweight.hpp>
+#include <boost/foreach.hpp>
+#include <boost/foreach_fwd.hpp>
+#include <boost/format.hpp>
+#include <boost/function.hpp>
+#include <boost/function_equal.hpp>
+#include <boost/iterator/function_output_iterator.hpp>
+#include <boost/functional.hpp>
+#include <boost/generator_iterator.hpp>
+#include <boost/geometry.hpp>
+#include <boost/get_pointer.hpp>
+#include <boost/gil.hpp>
+// Hana complains on msvc 19 parsed at least once from mingw gcc
+//#include <boost/hana.hpp>
+#include <boost/histogram.hpp>
+#include <boost/hof.hpp>
+#include <boost/implicit_cast.hpp>
+#include <boost/indirect_reference.hpp>
+#include <boost/integer.hpp>
+#include <boost/integer_fwd.hpp>
+#include <boost/integer_traits.hpp>
+#include <boost/intrusive_ptr.hpp>
+#include <boost/io_fwd.hpp>
+#include <boost/is_placeholder.hpp>
+#include <boost/iterator.hpp>
+#include <boost/iterator_adaptors.hpp>
+#include <boost/json.hpp>
+#include <boost/lambda2.hpp>
+#include <boost/leaf.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/limits.hpp>
+#include <boost/local_function.hpp>
+//creates warning about auto_ptr deprication may be an interaction with align.hpp, or others
+//#include <boost/locale.hpp>
+#include <boost/make_default.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/make_unique.hpp>
+#include <boost/math_fwd.hpp>
+#include <boost/mem_fn.hpp>
+#include <boost/memory_order.hpp>
+#include <boost/metaparse.hpp>
+#include <boost/mp11.hpp>
+// requires mpi
+//#include <boost/mpi.hpp>
+#include <boost/multi_array.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index_container_fwd.hpp>
+#include <boost/next_prior.hpp>
+#include <boost/shared_container_iterator.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/signals2.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/spirit/include/classic.hpp>
+#include <boost/stacktrace.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/static_string.hpp>
+#include <boost/swap.hpp>
+#include <boost/system.hpp>
+//#include <boost/thread.hpp>
+#include <boost/throw_exception.hpp>
+//#include <boost/timer/timer.hpp>
+#include <boost/token_functions.hpp>
+#include <boost/token_iterator.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/type.hpp>
+#include <boost/type_index.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/utility.hpp>
+#include <boost/variant.hpp>
+#include <boost/variant2.hpp>
+#include <boost/version.hpp>
+#include <boost/visit_each.hpp>
+//#include <boost/wave.hpp>
+#include <boost/weak_ptr.hpp>
+
+#endif  // THIRD_PARTY_BOOST_HEADERS_SEPARATE_COMPILATION_EXAMPLE_HPP_
---
+++ separate_compilation/boost_fwd.hpp
@@ -0,0 +1,184 @@
+//
+// Created by vis75817 on 7/29/2022.
+//
+
+#ifndef BOOST_HEADERS_SEPARATE_COMPILATION_BOOST_FWD_H_
+#define BOOST_HEADERS_SEPARATE_COMPILATION_BOOST_FWD_H_
+
+// here is a list of some forward declarations.
+
+#include <boost/accumulators/accumulators_fwd.hpp>
+#include <boost/accumulators/numeric/functional_fwd.hpp>
+#include <boost/accumulators/statistics_fwd.hpp>
+#include <boost/any/fwd.hpp>
+#include <boost/asio/basic_streambuf_fwd.hpp>
+#include <boost/asio/buffered_read_stream_fwd.hpp>
+#include <boost/asio/buffered_stream_fwd.hpp>
+#include <boost/asio/buffered_write_stream_fwd.hpp>
+#include <boost/asio/ts/netfwd.hpp>
+#include <boost/beast/websocket/stream_fwd.hpp>
+#include <boost/blank_fwd.hpp>
+#include <boost/circular_buffer_fwd.hpp>
+#include <boost/container/container_fwd.hpp>
+#include <boost/container/scoped_allocator_fwd.hpp>
+#include <boost/container/uses_allocator_fwd.hpp>
+#include <boost/container_hash/hash_fwd.hpp>
+#include <boost/dynamic_bitset_fwd.hpp>
+#include <boost/flyweight/assoc_container_factory_fwd.hpp>
+#include <boost/flyweight/flyweight_fwd.hpp>
+#include <boost/flyweight/hashed_factory_fwd.hpp>
+#include <boost/flyweight/intermodule_holder_fwd.hpp>
+#include <boost/flyweight/key_value_fwd.hpp>
+#include <boost/flyweight/no_locking_fwd.hpp>
+#include <boost/flyweight/no_tracking_fwd.hpp>
+#include <boost/flyweight/refcounted_fwd.hpp>
+#include <boost/flyweight/set_factory_fwd.hpp>
+#include <boost/flyweight/simple_locking_fwd.hpp>
+#include <boost/flyweight/static_holder_fwd.hpp>
+#include <boost/foreach_fwd.hpp>
+#include <boost/format/format_fwd.hpp>
+#include <boost/format/internals_fwd.hpp>
+#include <boost/function/function_fwd.hpp>
+#include <boost/functional/hash/hash_fwd.hpp>
+#include <boost/functional/hash_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/accumulate_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/fold_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/for_each_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/iter_fold_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/reverse_fold_fwd.hpp>
+#include <boost/fusion/algorithm/iteration/reverse_iter_fold_fwd.hpp>
+#include <boost/fusion/algorithm/query/find_fwd.hpp>
+#include <boost/fusion/algorithm/query/find_if_fwd.hpp>
+#include <boost/fusion/container/deque/deque_fwd.hpp>
+#include <boost/fusion/container/list/cons_fwd.hpp>
+#include <boost/fusion/container/list/list_fwd.hpp>
+#include <boost/fusion/container/map/map_fwd.hpp>
+#include <boost/fusion/container/set/set_fwd.hpp>
+#include <boost/fusion/container/vector/vector_fwd.hpp>
+#include <boost/fusion/include/deque_fwd.hpp>
+#include <boost/fusion/include/list_fwd.hpp>
+#include <boost/fusion/include/map_fwd.hpp>
+#include <boost/fusion/include/set_fwd.hpp>
+#include <boost/fusion/include/tag_of_fwd.hpp>
+#include <boost/fusion/include/tuple_fwd.hpp>
+#include <boost/fusion/include/vector_fwd.hpp>
+#include <boost/fusion/sequence/intrinsic_fwd.hpp>
+#include <boost/fusion/support/tag_of_fwd.hpp>
+#include <boost/fusion/tuple/tuple_fwd.hpp>
+#include <boost/fusion/view/joint_view/joint_view_fwd.hpp>
+#include <boost/fusion/view/repetitive_view/repetitive_view_fwd.hpp>
+#include <boost/fusion/view/transform_view/transform_view_fwd.hpp>
+#include <boost/fusion/view/zip_view/zip_view_iterator_fwd.hpp>
+#include <boost/gil/concepts/fwd.hpp>
+#include <boost/histogram/fwd.hpp>
+#include <boost/integer_fwd.hpp>
+#include <boost/interprocess/containers/containers_fwd.hpp>
+#include <boost/interprocess/interprocess_fwd.hpp>
+#include <boost/intrusive/intrusive_fwd.hpp>
+#include <boost/io_fwd.hpp>
+#include <boost/iostreams/operations_fwd.hpp>
+#include <boost/iostreams/traits_fwd.hpp>
+#include <boost/json/fwd.hpp>
+#include <boost/log/attributes/fallback_policy_fwd.hpp>
+#include <boost/log/attributes/value_extraction_fwd.hpp>
+#include <boost/log/attributes/value_visitation_fwd.hpp>
+#include <boost/log/expressions/attr_fwd.hpp>
+#include <boost/log/expressions/keyword_fwd.hpp>
+#include <boost/log/utility/formatting_ostream_fwd.hpp>
+#include <boost/log/utility/string_literal_fwd.hpp>
+#include <boost/log/utility/value_ref_fwd.hpp>
+#include <boost/logic/tribool_fwd.hpp>
+#include <boost/math/distributions/fwd.hpp>
+#include <boost/math/special_functions/math_fwd.hpp>
+#include <boost/math_fwd.hpp>
+//#include <boost/mpi/collectives_fwd.hpp>
+//#include <boost/mpi/datatype_fwd.hpp>
+//#include <boost/mpi/skeleton_and_content_fwd.hpp>
+#include <boost/mpl/advance_fwd.hpp>
+#include <boost/mpl/apply_fwd.hpp>
+#include <boost/mpl/arg_fwd.hpp>
+#include <boost/mpl/at_fwd.hpp>
+#include <boost/mpl/aux_/na_fwd.hpp>
+#include <boost/phoenix/core/terminal_fwd.hpp>
+#include <boost/poly_collection/any_collection_fwd.hpp>
+#include <boost/poly_collection/base_collection_fwd.hpp>
+#include <boost/poly_collection/function_collection_fwd.hpp>
+#include <boost/pool/poolfwd.hpp>
+#include <boost/property_tree/ptree_fwd.hpp>
+#include <boost/proto/proto_fwd.hpp>
+// seems to want python headers avaliable.
+//#include <boost/python/args_fwd.hpp>
+//#include <boost/python/class_fwd.hpp>
+//#include <boost/python/handle_fwd.hpp>
+//#include <boost/python/object/value_holder_fwd.hpp>
+//#include <boost/python/object_fwd.hpp>
+#include <boost/range/adaptor/argument_fwd.hpp>
+#include <boost/range/range_fwd.hpp>
+#include <boost/ratio/ratio_fwd.hpp>
+#include <boost/regex/v4/regex_fwd.hpp>
+#include <boost/regex/v5/regex_fwd.hpp>
+#include <boost/regex_fwd.hpp>
+#include <boost/serialization/void_cast_fwd.hpp>
+#include <boost/spirit/home/classic/attribute/closure_fwd.hpp>
+#include <boost/spirit/home/classic/core/non_terminal/subrule_fwd.hpp>
+#include <boost/spirit/home/classic/core/primitives/numerics_fwd.hpp>
+#include <boost/spirit/home/classic/core/scanner/scanner_fwd.hpp>
+#include <boost/spirit/home/classic/core/scanner/skipper_fwd.hpp>
+#include <boost/spirit/home/classic/dynamic/stored_rule_fwd.hpp>
+#include <boost/spirit/home/classic/error_handling/exceptions_fwd.hpp>
+#include <boost/spirit/home/classic/iterator/file_iterator_fwd.hpp>
+#include <boost/spirit/home/classic/iterator/multi_pass_fwd.hpp>
+#include <boost/spirit/home/classic/iterator/position_iterator_fwd.hpp>
+#include <boost/spirit/home/classic/symbols/symbols_fwd.hpp>
+#include <boost/spirit/home/classic/tree/ast_fwd.hpp>
+#include <boost/spirit/home/classic/tree/common_fwd.hpp>
+#include <boost/spirit/home/classic/tree/parse_tree_fwd.hpp>
+#include <boost/spirit/home/classic/utility/confix_fwd.hpp>
+#include <boost/spirit/home/classic/utility/distinct_fwd.hpp>
+#include <boost/spirit/home/classic/utility/escape_char_fwd.hpp>
+#include <boost/spirit/home/classic/utility/grammar_def_fwd.hpp>
+#include <boost/spirit/home/classic/utility/lists_fwd.hpp>
+#include <boost/spirit/home/karma/nonterminal/nonterminal_fwd.hpp>
+#include <boost/spirit/home/qi/nonterminal/nonterminal_fwd.hpp>
+#include <boost/spirit/home/support/attributes_fwd.hpp>
+#include <boost/spirit/home/support/iterators/multi_pass_fwd.hpp>
+#include <boost/spirit/home/support/utree/utree_traits_fwd.hpp>
+#include <boost/spirit/include/classic_ast_fwd.hpp>
+#include <boost/spirit/include/classic_closure_fwd.hpp          >
+#include <boost/spirit/include/classic_common_fwd.hpp           >
+#include <boost/spirit/include/classic_confix_fwd.hpp           >
+#include <boost/spirit/include/classic_distinct_fwd.hpp         >
+#include <boost/spirit/include/classic_escape_char_fwd.hpp      >
+#include <boost/spirit/include/classic_exceptions_fwd.hpp       >
+#include <boost/spirit/include/classic_file_iterator_fwd.hpp    >
+#include <boost/spirit/include/classic_grammar_def_fwd.hpp  >
+#include <boost/spirit/include/classic_lists_fwd.hpp            >
+#include <boost/spirit/include/classic_multi_pass_fwd.hpp       >
+#include <boost/spirit/include/classic_numerics_fwd.hpp         >
+#include <boost/spirit/include/classic_parse_tree_fwd.hpp       >
+#include <boost/spirit/include/classic_position_iterator_fwd.hpp>
+#include <boost/spirit/include/classic_scanner_fwd.hpp          >
+#include <boost/spirit/include/classic_skipper_fwd.hpp          >
+#include <boost/spirit/include/classic_stored_rule_fwd.hpp      >
+#include <boost/spirit/include/classic_subrule_fwd.hpp          >
+#include <boost/spirit/include/classic_symbols_fwd.hpp          >
+#include <boost/spirit/include/support_attributes_fwd.hpp       >
+#include <boost/spirit/include/support_multi_pass_fwd.hpp       >
+#include <boost/stacktrace/stacktrace_fwd.hpp>
+#include <boost/stl_interfaces/fwd.hpp>
+#include <boost/test/data/monomorphic/fwd.hpp>
+#include <boost/test/utils/basic_cstring/basic_cstring_fwd.hpp>
+#include <boost/test/utils/runtime/fwd.hpp>
+// not applicable on windows, and not likely to be useful in header only.
+//#include <boost/thread/pthread/condition_variable_fwd.hpp>
+#include <boost/units/units_fwd.hpp>
+#include <boost/unordered/unordered_map_fwd.hpp>
+#include <boost/unordered/unordered_set_fwd.hpp>
+#include <boost/utility/string_ref_fwd.hpp>
+#include <boost/utility/string_view_fwd.hpp>
+#include <boost/variant/recursive_wrapper_fwd.hpp>
+#include <boost/variant/variant_fwd.hpp>
+#include <boost/xpressive/xpressive_fwd.hpp>
+#include <boost/yap/algorithm_fwd.hpp>
+
+#endif  // MIDDLEWARE_THIRD_PARTY_BOOST_HEADERS_SEPARATE_COMPILATION_BOOST_FWD_H_
